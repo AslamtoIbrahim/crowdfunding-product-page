893129c47c6e80a4068f65030c9a87f9a79787b6
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-01-28 19:25:19 +0100

    add header background

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header className="bg-hero-mobile md:bg-hero-desktop bg-no-repeat bg-center bg-cover w-full  h-[25rem]">
+        
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +13,13 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        commissioner: ['Commissioner', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-desktop' : "url('../../images/image-hero-desktop.jpg')",
+        'hero-mobile' : "url('../../images/image-hero-mobile.jpg')",
+      }
     },
   },
   plugins: [],
